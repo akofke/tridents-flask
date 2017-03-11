6c010f8c7ae168ec6b04851cb75b5548b2f30884
Alex Kofke <akofke@gmail.com>
2017-03-10 22:38:02 -0500

    add sqlalchemy and postgres db
    Signed-off-by: Alex Kofke <akofke@gmail.com>

---
+++ tridents/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from .tridents import db
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(80))
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+    publish_date = db.Column(db.DateTime)
+
+    def __init__(self, author, title, body):
+        self.author = author
+        self.title = title
+        self.body = body
+        self.publish_date = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
--- tridents/tridents.py
+++ tridents/tridents.py
@@ -6,6 +6,8 @@ import requests
 
 
 # create the application instance
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 # load config from this file
@@ -14,11 +16,14 @@ app.config.from_object(__name__)
 # default config
 app.config.update(
     SECRET_KEY="development key",
+    SQLALCHEMY_DATABASE_URI="postgresql://localhost/tridents_dev",
     AUTH0_CALLBACK_URL="http://localhost:5000/callback"
 )
 
 # load additional config from the file at FLASK_CONFIG
 app.config.from_envvar('FLASK_CONFIG', silent=False)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/callback')
